e68aaac95feaff9e3a136233b63dff84f23df6ac
Qian Liu <liu.qian.am@gmail.com>
2017-08-15 19:03:14 -0700

    update

--- 11-searching/11.2-search-a-sorted-array-for-entry-equal-to-its-index/C++/C++/main.cpp
+++ 11-searching/11.2-search-a-sorted-array-for-entry-equal-to-its-index/C++/C++/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cassert>
 
+using std::vector;
+
+
+int SearchIndex(vector<int>& A){
+    int left = 0, right = A.size() - 1;
+    
+    //if mid < A[mid], have to go right
+    //if mid > A[mid], have to go left
+    //if mid == A[mid], return
+    
+    while(left <= right){
+        int mid = left + (right - left)/2;
+        if(mid < A[mid]){
+            left = mid + 1;
+        }else if(mid > A[mid]){
+            right = mid - 1;
+        }else{
+            return mid;
+        }
+    }
+    
+    return -1;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<int> A = {-1,0,1,3,5,6,7};
+    int result = SearchIndex(A);
+    assert(result == 3);
     return 0;
 }
